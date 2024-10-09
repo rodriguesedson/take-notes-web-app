8f9e12fa4c19e24685dac560570bc1f62059eb0e
Edson Rodrigues <rodriguesedson.dev@gmail.com>
2024-10-09 15:36:43 -0300

    feat: create updateNote endpoint

--- src/enum/Enum.js
+++ src/enum/Enum.js
@@ -3,6 +3,9 @@ class Enum {
   static created = new Enum('Created successfully.');
   static missingInfo = new Enum('All fields must be passed.');
   static notFound = new Enum('Note not found. Try another parameter.');
+  static noIdPassed = new Enum('No id was passed.');
+  static noValuesUpdate = new Enum('No values passed to update');
+  static updateSuccess = new Enum('Updated successfully');
 
   constructor(type) {
     this.type = type;
